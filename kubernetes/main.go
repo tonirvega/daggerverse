f78b4b4a49ca69cfa5ef2d19bac14d5f91cf0d58
tonirvega <arvegadev@hotmail.com>
2024-11-09 17:13:09 +0100

    working...

--- kubernetes/main.go
+++ kubernetes/main.go
@@ -3,36 +3,30 @@ package main
 import (
 	"context"
 	"dagger/kubernetes/internal/dagger"
+	"time"
 )
 
 type Kubernetes struct{}
 
-func (m *Kubernetes) Run(ctx context.Context) *dagger.Container {
-
-	dindSvc := dag.Container().
-		From("docker:dind").
-		WithUser("root").
-		WithEnvVariable("DOCKER_TLS_CERTDIR", "").
-		WithExec([]string{"-H", "tcp://0.0.0.0:2375"}, dagger.ContainerWithExecOpts{
-			UseEntrypoint:            true,
-			InsecureRootCapabilities: true}).
-		WithExposedPort(2375).AsService()
-
-	endpoint, err := dindSvc.Endpoint(ctx, dagger.ServiceEndpointOpts{Scheme: "tcp"})
-
-	if err != nil {
-
-		panic(err)
-
-	}
+// dagger call run-sock --docker-sock=/var/run/docker.sock --kind-svc=tcp://127.0.0.1:3000 terminal stdout
+func (m *Kubernetes) Run(ctx context.Context, dockerSock *dagger.Socket, kindSvc *dagger.Service) *dagger.Container {
 
 	return dag.Container().
 		From("alpine").
-		WithEnvVariable("DOCKER_HOST", endpoint).
-		WithServiceBinding("dind", dindSvc).
+		WithUnixSocket("/var/run/docker.sock", dockerSock).
+		WithFile("kind.yaml", dag.CurrentModule().Source().File("kind.yaml")).
 		WithExec([]string{"apk", "add", "docker", "kubectl", "k9s", "curl"}).
 		WithExec([]string{"curl", "-Lo", "./kind", "https://kind.sigs.k8s.io/dl/v0.25.0/kind-linux-amd64"}).
 		WithExec([]string{"chmod", "+x", "./kind"}).
-		WithExec([]string{"mv", "./kind", "/usr/local/bin/kind"}, dagger.ContainerWithExecOpts{InsecureRootCapabilities: true}).
-		WithExec([]string{"kind", "create", "cluster"}, dagger.ContainerWithExecOpts{InsecureRootCapabilities: true})
+		WithExec([]string{"mv", "./kind", "/usr/local/bin/kind"}).
+		WithEnvVariable("BUST", time.Now().String()).
+		WithExec([]string{"kind", "delete", "cluster"}).
+		WithExec([]string{
+			"kind", "create", "cluster",
+			"--config", "kind.yaml", "--wait", "30s",
+		}, dagger.ContainerWithExecOpts{
+			InsecureRootCapabilities: true,
+		}).
+		WithServiceBinding("localhost", kindSvc).
+		WithExec([]string{"kubectl", "get", "nodes", "--server=https://localhost:3000"})
 }
