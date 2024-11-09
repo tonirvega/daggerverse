b103a3050501f3a7ab1160a3cd27dfeea8f0bccf
Toni R. Vega <91343444+tonirvega@users.noreply.github.com>
2024-11-09 14:02:47 +0100

    Update main.go

--- kubernetes/main.go
+++ kubernetes/main.go
@@ -1,17 +1,3 @@
-// A generated module for Kubernetes functions
-//
-// This module has been generated via dagger init and serves as a reference to
-// basic module structure as you get started with Dagger.
-//
-// Two functions have been pre-created. You can modify, delete, or add to them,
-// as needed. They demonstrate usage of arguments and return types using simple
-// echo and grep commands. The functions can be called from the dagger CLI or
-// from one of the SDKs.
-//
-// The first line in this comment block is a short description line and the
-// rest is a long description with more detail on the module's purpose or usage,
-// if appropriate. All modules should have a short description.
-
 package main
 
 import (
@@ -45,8 +31,6 @@ func (m *Kubernetes) Run(ctx context.Context) *dagger.Container {
 		From("alpine").
 		WithEnvVariable("DOCKER_HOST", endpoint).
 		WithServiceBinding("dind", dindSvc).
-		// WithFile("cgroups.sh", dag.CurrentModule().Source().File("cgroups.sh")).
-		// WithFile("Dockerfile", dag.CurrentModule().Source().File("Dockerfile")).
 		WithExec([]string{"apk", "add", "docker", "kubectl", "k9s", "curl"}).
 		WithExec([]string{"curl", "-Lo", "./kind", "https://kind.sigs.k8s.io/dl/v0.25.0/kind-linux-amd64"}).
 		WithExec([]string{"chmod", "+x", "./kind"}).
